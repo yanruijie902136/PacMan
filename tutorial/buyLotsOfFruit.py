7b05a6a0c1bc8e49b36dffa20b277a60c7a6aa1d
yanruijie902136 <yanruijie902136@gmail.com>
2024-07-25 13:08:17 +0800

    Complete project 0

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -34,9 +34,8 @@ def buyLotsOfFruit(orderList):
 
     Returns cost of order
     """
-    totalCost = 0.0
     "*** YOUR CODE HERE ***"
-    return totalCost
+    return sum([fruitPrices[fruit] * numPounds for fruit, numPounds in orderList])
 
 
 # Main Method
